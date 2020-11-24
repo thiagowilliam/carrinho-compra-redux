8a6d13ae974a2a006277a9314408ae7679676fe4
Thiago William <thiago.will.silva@gmail.com>
2020-11-23 22:13:49 -0300

    Add items no carrinho usando redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+
+import store from './store'
+import Catalog from './components/Catalog';
+import Cart from './components/Cart';
 
 function App() {
   return (
-    <h1>Hello Word</h1>
+    <Provider store={store}>
+      <Catalog />
+      <Cart />
+    </Provider>
   );
 }
 
